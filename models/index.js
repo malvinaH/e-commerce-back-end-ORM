0ae57959faeb81a3bfd1d1a26c37c3e009cdfedd
Malvina Hasa <hasamalvina24@gmail.com>
2022-11-07 10:43:18 -0500

    created connection.js schema file and all models

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+
+module.exports = {
+    Product,
+    Category,
+    Tag,
+    ProductTag,
+  };
